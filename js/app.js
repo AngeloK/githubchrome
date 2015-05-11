37f846cd6d1a063593fccfc0f5eef6b121735921
Neyanbhbin <neyanbhbin.kang@gmail.com>
2015-05-11 10:27:39 +0800

    github service tset

--- js/app.js
+++ js/app.js
@@ -1,13 +1,5 @@
-var myApp = angular.module('demoApp', ['ngRoute'])
-.controller("MainController", function($scope, $timeout){
-    $scope.time = {};
-
-
-    var updateTime = function(){
-        $scope.time.raw = new Date();
-        $timeout(updateTime,1000);
-    };
-
-
-    updateTime();
-})
+var myApp = angular.module('demoApp', [
+                'ngRoute',
+                'controlllers',
+                'services'
+]);
