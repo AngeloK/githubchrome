7500cc9f15c0a51a9327dac1199d4cbb2aada826
Neyanbhbin <neyanbhbin.kang@gmail.com>
2015-05-11 19:38:31 +0800

    stylish result item with githubService working

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -1,26 +1,16 @@
 angular.module('demoApp',['services'])
-.controller('MainController',function($scope,$timeout){
+.controller('MainController',function($scope,$timeout,githubSearch){
     $scope.time = {};
     $scope.search = {};
+    
+    //githubSearch.getSearchResult("tornado","stars","desc")
+    //.then(function(data){
+        //$scope.items = data.data.items;
+    //});
 
     var updateTime = function(){
         $scope.time.raw = new Date();
         $timeout(updateTime,1000);
     }
     updateTime();
-})
-.directive('githubResult',function(){
-    return {
-        restrict:'A',
-        //replace:true,
-        //template:"<div>hello world</div>"
-        templateUrl:'templates/github_item.html'
-        //scope: {
-            //title:"django/django",
-            //description:"The web framework",
-            //language:"Python",
-            //stars:"14,163",
-            //forks:"5,629"
-        //}
-    } 
-})
+});
