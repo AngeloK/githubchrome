cb3f121bc8088fb1b0997a8538c5635e22835834
Neyanbhbin <neyanbhbin.kang@gmail.com>
2015-05-13 17:17:59 +0800

    hidden search div before submit

--- js/directives/git_search_form.js
+++ js/directives/git_search_form.js
@@ -1,8 +1,18 @@
 angular.module('directives',[])
 .directive('githubSearchForm',function($animate){
-    return function($scope, element, attrs){
-        $scope.$on('form submit', function(){
+    function link(scope, element, attrs){
+        scope.$on('form submit', function(){
             $animate['addClass'](element, 'ng-move');
         })
     }
-});
+    return {
+        link:link
+    }
+})
+.directive('githubSearchResult',function($animate){
+    return function(scope, element, attrs){
+        scope.$on('form submit', function(){
+            $animate['removeClass'](element,'hidden');
+        })
+    }
+})
